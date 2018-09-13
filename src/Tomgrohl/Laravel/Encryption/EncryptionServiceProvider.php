503d995c5e5f26fc7f31accb72d59dc82ad8ff6a
Martijn Kriek <martijn.kriek@relax-software.nl>
2018-09-13 16:00:44 +0200

    Define MCRYPT_RIJNDAEL_128 and MCRYPT_MODE_CBC if not defined, for use without ext-mcrypt.

--- src/Tomgrohl/Laravel/Encryption/EncryptionServiceProvider.php
+++ src/Tomgrohl/Laravel/Encryption/EncryptionServiceProvider.php
@@ -11,10 +11,16 @@ use Illuminate\Support\ServiceProvider;
  */
 class EncryptionServiceProvider extends ServiceProvider
 {
+
     public function register()
     {
-        $this->app->bindShared('encrypter', function($app) {
+        $this->app->bindShared('encrypter', function ($app) {
 
+            // Before creating an Encrypter, we need to make sure
+            // the constants MCRYPT_RIJNDAEL_128 and MCRYPT_MODE_CBC are set,
+            // because the base class (Illuminate\Encryption\Encrypter) uses them.
+            self::defineIfNotDefined('MCRYPT_RIJNDAEL_128', "rijndael-128");
+            self::defineIfNotDefined('MCRYPT_MODE_CBC', "cbc");
 
             if ($app['config']->has('app.cipher')) {
 
@@ -22,12 +28,19 @@ class EncryptionServiceProvider extends ServiceProvider
                     $app['config']['app.key'],
                     $app['config']['app.cipher']
                 );
-            }
-            else {
+            } else {
                 return new Encrypter($app['config']['app.key']);
             }
         });
 
         $this->app->alias('encrypter', Encrypter::class);
     }
+
+    private static function defineIfNotDefined($name, $value)
+    {
+        if (!defined($name)) {
+            define($name, $value);
+        }
+    }
+
 }
